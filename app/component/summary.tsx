f7999ed7d1158770857d7d65fd3e3059a0bd9575
elamaran-gh <elamaran1928@gmail.com>
2025-12-15 22:20:54 +0530

    ScoreGauge.tsx

--- app/component/summary.tsx
+++ app/component/summary.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const summary = () => {
   return (
-    <div>summary</div>
+    <div className="bg-white rounded-2xl shadow-md w-full">
+        <div className="flex flex-row items-center p-4 gap-8">
+
+        </div>
+
+    </div>
   )
 }
 
